60ab8829e0c502e79c6f529d5cd15993e02f1b9d
Joseph <jgaynier2014@gmail.com>
2020-12-07 14:16:23 -0500

    change last localhost url

--- src/Visuals/Visuals.js
+++ src/Visuals/Visuals.js
@@ -107,7 +107,7 @@ export async function getBudget() {
   const localData = { userId };
   const token = localStorage.getItem('jwt');
   var results;
-  await axios.post("http://localhost:3000/budget/get", localData, {
+  await axios.post("https://personal-budget-final-ednzw.ondigitalocean.app/personal-b-final-server:8080/budget/get", localData, {
       headers: {
           'Authorization': `Bearer ${token}`
       }
